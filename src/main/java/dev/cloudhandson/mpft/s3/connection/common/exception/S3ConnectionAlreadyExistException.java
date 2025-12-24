fcb0eb760fc46f3f46b30d90528debc5ced83f42
Thakur13 <asthakur185@gmail.com>
2025-12-24 13:09:57 +0000

    Added S3 Connection Crud Operations

---
+++ src/main/java/dev/cloudhandson/mpft/s3/connection/common/exception/S3ConnectionAlreadyExistException.java
@@ -0,0 +1,7 @@
+package dev.cloudhandson.mpft.s3.connection.common.exception;
+
+public class S3ConnectionAlreadyExistException extends RuntimeException {
+    public S3ConnectionAlreadyExistException(String message) {
+        super(message);
+    }
+}
